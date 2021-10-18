4de18cb620869f187a6fa3a6aa47e0309021dc66
Kaustav Ghosh <teetangh@gmail.com>
2021-10-18 10:51:07 +0530

    updated transformer embeddings

---
+++ dl-natural-language-processing/basic_transformer/custom_embeddings.py
@@ -0,0 +1,14 @@
+import os
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer                                         
+from sklearn.feature_extraction.text import TfidfVectorizer 
+
+class GloveModel():
+    def __init__():
+        pass
+
+class Word2VecModel():
+    def __init__():
+        pass
+
--- dl-natural-language-processing/basic_transformer/transformer.py
+++ dl-natural-language-processing/basic_transformer/transformer.py
@@ -1,3 +1,9 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import string
 import os
 import numpy as np
 import pandas as pd
@@ -7,22 +13,17 @@ import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
 
-import string
-
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
 
 #import tensorflow as tf
 #from tensorflow.keras.layers import Dense,Dropout
-from sklearn.feature_extraction.text import CountVectorizer
+
 
 class InputEmbedding():
-    
-    def __init__(self,corpus):
+
+    def __init__(self, corpus):
         self.input_corpus = corpus
 
-    def clean_text(self,df):
+    def clean_text(self, df):
         all_text = list()
         lines = df
         PS = PorterStemmer()  # Takes only the root words
@@ -47,11 +48,11 @@ class InputEmbedding():
             text = re.sub(r"i'm", "i am", text)
             text = re.sub(r"he's", "he is", text)
             text = re.sub(r"she's", "she is", text)
-            text = re.sub(r"that's", "that is", text)        
+            text = re.sub(r"that's", "that is", text)
             text = re.sub(r"what's", "what is", text)
-            text = re.sub(r"where's", "where is", text) 
-            text = re.sub(r"\'ll", " will", text)  
-            text = re.sub(r"\'ve", " have", text)  
+            text = re.sub(r"where's", "where is", text)
+            text = re.sub(r"\'ll", " will", text)
+            text = re.sub(r"\'ve", " have", text)
             text = re.sub(r"\'re", " are", text)
             text = re.sub(r"\'d", " would", text)
             text = re.sub(r"\'ve", " have", text)
@@ -62,9 +63,8 @@ class InputEmbedding():
             text = re.sub(r"it's", "it is", text)
             text = re.sub(r"couldn't", "could not", text)
             text = re.sub(r"have't", "have not", text)
-            
-            text = re.sub(r"[,.\"!@#$%^&*(){}?/;`~:<>+=-]", "", text)
 
+            text = re.sub(r"[,.\"!@#$%^&*(){}?/;`~:<>+=-]", "", text)
 
             # tokenize over split return list rather than strings
             tokens = word_tokenize(text)
@@ -82,54 +82,70 @@ class InputEmbedding():
             # words = [w for w in words if not w in stop_words]
 
             # Use Porter Stemmer to stem the words to root words (necessary for sentiment analysis and text processing)
-            # words = [PS.stem(w) for w in words if not w in stop_words] 
+            # words = [PS.stem(w) for w in words if not w in stop_words]
             words = ' '.join(words)  # join the tokens (make a string)
             all_text.append(words)  # add it to the large list
 
         return all_text
 
-    def get_postition_encoding(self,all_text):
+    def get_word_embeddings(seflf):
+        pass
+
+    def get_postition_encoding(self, all_text):
         pass
 
     def run(self):
         self.cleaned_text = self.clean_text(self.input_corpus)
-        self.tokenized_sentences = [sentence.split() for sentence in self.cleaned_text]
-        self.position_encoded_text = get_position_encoding(self.tokenized_sentences)
+        self.tokenized_sentences = [sentence.split()
+                                    for sentence in self.cleaned_text]
+        self.input_embeddings = get_word_embeddings(self.tokenized_sentences)
+
+        # self.position_encoded_text = get_position_encoding(
+        #     self.tokenized_sentences)
+
 
 class ScaledDotProdcutAttention():
     def __init__(self):
         pass
 
+
 class MultiHeadAttention():
     def __init__(self):
         pass
+
 
 class MaskedMultiHeadAttention():
     def __init__(self):
         pass
 
+
 class Encoder():
     def __init__(self):
         pass
+
 
 class Decoder():
     def __init__(self):
         pass
 
+
 class Transformer():
     def __init__(self):
         pass
 
+
 def main():
     #EMBEDDING_DIR = ...
     DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-    data = pd.read_csv(os.path.join(DIR_PATH,"data/rus.txt"),sep="\t",header=None)
-    data = data.iloc[:,0:2]
+    data = pd.read_csv(os.path.join(
+        DIR_PATH, "data/rus.txt"), sep="\t", header=None)
+    data = data.iloc[:, 0:2]
     corpus = data[0].to_list()
-    
+
     input_embeddings = InputEmbedding(corpus)
     input_embeddings.run()
     print(input_embeddings.tokenized_sentences)
+
 
 if __name__ == "__main__":
     main()
